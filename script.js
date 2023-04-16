f8415aa28cdce51d77edf98cd90e31027937ba4a
suraj sharma <94784024+surajsharmacode@users.noreply.github.com>
2023-04-16 10:21:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 // complete the given function
 
 function palindrome(str){
+	let newstr=str.toLowerCase();
+	let left=0;
+	let right=str.length-1;
+	while(left<right){
+		if(newstr[left]!==newstr[right]) return false;
+		left++;
+		right--;
+	}
+	return true;
 
 }
 module.exports = palindrome
